52cce45607e1f9c7ab30eec1e1e3953dfe333a88
TahirMurtaza <tahirmurtaza5152@gmail.com>
2026-07-06 16:23:19 +0500

    Remove wave emoji from WhatsApp message

--- src/components/utilities/WhatsAppFloat.tsx
+++ src/components/utilities/WhatsAppFloat.tsx
@@ -1,7 +1,7 @@
 const WhatsAppFloat = () => {
     return (
         <a
-            href="https://wa.me/923322358961?text=Hey%20Tahir%20%F0%9F%91%8B%20Just%20checked%20out%20your%20portfolio%20%E2%80%94%20got%20a%20minute%20to%20chat%3F"
+            href="https://wa.me/923322358961?text=Hey%20Tahir%2C%20just%20checked%20out%20your%20portfolio%20%E2%80%94%20got%20a%20minute%20to%20chat%3F"
             target="_blank"
             rel="noopener noreferrer"
             className="whatsapp-float"
